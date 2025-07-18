655533e817d8f3b813e619d06feabf3e13faf8d5
SajidKhan71 <sajid7271.sk@gmail.com>
2025-07-18 20:10:54 +0600

    Update index.js

--- index.js
+++ index.js
@@ -212,7 +212,8 @@ app.post("/auth/login", async (req, res) => {
       httpOnly: true,
       secure: true,  
       sameSite: "none",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      partitioned: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.json({ success: true, email: decoded.email });
