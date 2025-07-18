defd3cb7dfe9f93e5e84a8c952560c4b2f2e1c38
SajidKhan71 <sajid7271.sk@gmail.com>
2025-07-18 20:08:18 +0600

    Update index.js

--- index.js
+++ index.js
@@ -210,6 +210,8 @@ app.post("/auth/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
+      secure: true,  
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
