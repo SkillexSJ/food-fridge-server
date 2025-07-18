c75238c1976211eb47ac161ce0eafc44aea0b027
SajidKhan71 <sajid7271.sk@gmail.com>
2025-07-18 20:05:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const uri = process.env.MONGO_URI;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://food-fridge-8be96.web.app",
     credentials: true,
   })
 );
